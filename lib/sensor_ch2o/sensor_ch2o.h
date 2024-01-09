9d44f2712106c957b6bfe6b05ec1a0c557631c92
calvin0705 <40193716+calvin0705@users.noreply.github.com>
2024-01-09 09:54:03 +0800

    Add CH2O sensor function in project

---
+++ lib/sensor_ch2o/sensor_ch2o.h
@@ -0,0 +1,6 @@
+#include <Arduino.h>
+#include <iostream>
+
+float ch2o_data_transfer();
+void task_ch2o(String topic_sn);
+void clear_uart_buffer();
--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,7 @@ using namespace std;
 #include <sensor_cmd.h>
 #include <sensor_co2.h>
 #include <sensor_co.h>
+#include <sensor_ch2o.h>
 
 Scheduler runner;
 
@@ -518,9 +519,9 @@ void t2Callback() {
   }
 
   // task_co2(topic_sn);
-  task_co(topic_sn);
+  // task_co(topic_sn);
   // task_pm25(topic_sn);
-  // task_ch2o(topic_sn);
+  task_ch2o(topic_sn);
   
   Serial.println("t2 ======================");
 }
